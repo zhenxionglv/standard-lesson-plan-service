862f25aed7ed7dac8858c6b284ce0fed9229f130
JY29 <344159020@qq.com>
2017-03-17 09:56:33 +0800

    change model

--- models/standard-lesson-plan.js
+++ models/standard-lesson-plan.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  title: String,
-  num: String,
-  target: String,
-  grade: String,
-  method: String,
-  createAt: Date,
-  updateAt: Date,
-  createBy: String,
+  title: String, // 标题
+  target: String, // 教学内容与目标
+  category: String, // 运动分类id
+  grade: String, // 年级
+  method: String, // 教学组织与方法
+  createAt: { type: Date, default: new Date() }, // 创建时间
+  createBy: String, //创建人：运营帐号id
 })
 
 module.exports = mongoose.model('standard_lesson_plan', schema);
