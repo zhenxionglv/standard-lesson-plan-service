4a454cc001698161185f40f6948b2d4d1e23c7df
FengYuHe <heyufeng8080@gmail.com>
2017-05-18 16:10:37 +0800

    add consul

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const seneca = require('seneca');
 const nconf = require('nconf');
+const Consul = require('xyj-consul');
 
 // 根据环境变量获取config文件
 nconf.argv().env();
@@ -13,4 +14,9 @@ const register = require('./register/register');
 // 启动服务
 module.exports = seneca({ log: { level: nconf.get('senecaLogLevel') } })
   .use(register)
-  .listen({ type: 'http', port: nconf.get('senecaPort') });
+  .listen({ type: 'http', port: nconf.get('senecaPort') })
+  .ready(() => {
+    // consul服务
+    const consul = new Consul(nconf.get('consul'));
+    consul.register(nconf.get('consul'));
+  });
